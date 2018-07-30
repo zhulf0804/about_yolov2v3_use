dfbad2113fbec50cc4f02300f258888053ec0194
zhulf <zhulf0804@gmai.com>
2018-07-30 17:24:12 +0800

    edit rotate.py

--- rotate.py
+++ rotate.py
@@ -36,13 +36,13 @@ def rotate_label(file_name):
     # label x y w h
 
     # 90
-    # (x, y) -> (1 - y ,x)
+    # (x, y) -> (y ,1 - x)
     # (w, h) -> (h, w)
     # 180
     # (x, y) -> (1 - x, 1 - y)
     # (w, h) -> (w, h)
     # 270
-    # (x, y) -> (y, 1 - x)
+    # (x, y) -> (1 - y, x)
     # (w, h) -> (h, w)
 
     for line in f.readlines():
@@ -52,9 +52,9 @@ def rotate_label(file_name):
         w = float(z[3])
         h = float(z[4])
 
-        label_90 = z[0] + " " + str(1 - y) + " " + str(x) + " " + str(h) + " " + str(w) + "\n"
+        label_90 = z[0] + " " + str(y) + " " + str(1 - x) + " " + str(h) + " " + str(w) + "\n"
         label_180 = z[0] + " " + str(1 - x) + " " + str(1 - y) + " " + str(w) + " " + str(h) + "\n"
-        label_270 = z[0] + " " + str(y) + " " + str(1 - x) + " " + str(h) + " " + str(w) + "\n"
+        label_270 = z[0] + " " + str(1 - y) + " " + str(x) + " " + str(h) + " " + str(w) + "\n"
 
         f_90.write(label_90)
         f_180.write(label_180)
@@ -64,21 +64,25 @@ def rotate_label(file_name):
     f_270.close()
 
 
-img_dirname = "./datasets/train/JPEGImages/"
-label_dirname = "./datasets/train/labels"
+img_dirname = "./daimule_30_classes_rotate/train/JPEGImages/"
+label_dirname = "./daimule_30_classes_rotate/train/labels"
 
-filelist_name = "./datasets/filelist/all.txt"
+filelist_name = "./daimule_30_classes_rotate/filelist/all.txt"
 
-
-for img_file in os.listdir(img_dirname):
-    rotate_image(img_file)
-    print img_file + " rotates ok!"
-
+#index = 1
+#for img_file in os.listdir(img_dirname):
+#    if img_file.endswith(".jpg"):
+#        img_file = os.path.join(img_dirname, img_file)
+#        rotate_image(img_file)
+#        print str(index) + ", " + img_file + " rotates ok!"
+#        index = index + 1
 
 for label_file in os.listdir(label_dirname):
-    rotate_label(label_file)
-    print label_file + " rotates ok!"
+    if label_file.endswith(".txt"):
+        label_file = os.path.join(label_dirname, label_file)
+        rotate_label(label_file)
+        print label_file + " rotates ok!"
 
-#rotate_label('./1.txt')
-#img_name = "./1.jpg"
+#rotate_label('./IMG_20180313_152256.txt')
+#img_name = "./IMG_20180313_152256.jpg"
 #rotate_image(img_name)
